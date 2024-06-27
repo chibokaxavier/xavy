81c1bdd75278e363eec1742870ed8574db1b4bcf
manlikexavy <chibokaxavier@gmail.com>
2024-06-27 22:55:06 +0100

    DONE WITH THE ANIMATIONS UPMNEXT IS THE NAVBAR FOR THE MOBILE VIEW

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,30 @@
-import Link from 'next/link'
-import React from 'react'
-import Nav from "@/components/Nav"
+import Link from "next/link";
+import React from "react";
+import Nav from "@/components/Nav";
+import { Button } from "./ui/button";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
-    return (
-        <header className='py-8 xl:py-12 text-white'>
-            <div className='container mx-auto flex justify-between items-center'>
-                <Link href={'/'}>
-                    <h1 className='text-4xl font-semibold'>
-                        Xavier<span className='text-accent'>.</span>
-                    </h1>
-                </Link>
-                <div className='hidden xl:flex gap-8 justify-center items-center'>
-                    <Nav />
-                    <Link href={'/contact'}><button>Hire me </button></Link>
-                </div>
-                <div className='xl:hidden'>
-                    mobile nav
-                </div>
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href={"/"}>
+          <h1 className="text-4xl font-semibold">
+            Xavier<span className="text-accent">.</span>
+          </h1>
+        </Link>
+        <div className="hidden xl:flex gap-8 justify-center items-center">
+          <Nav />
+          <Link href={"/contact"}>
+            <Button>Hire me </Button>
+          </Link>
+        </div>
+        <div className="xl:hidden">
+          <MobileNav />
+        </div>
+      </div>
+    </header>
+  );
+};
 
-            </div>
-        </header>
-    )
-}
-
-export default Header
+export default Header;
--- components/StairTransition.tsx
+++ components/StairTransition.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, easeInOut, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 import Stairs from "./Stairs";
 
@@ -12,6 +12,15 @@ const StairTransition = () => {
           <div className="w-screen h-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex ">
             <Stairs />
           </div>
+
+          <motion.div
+            className="w-screen h-screen fixed bg-primary top-0 pointer-events-none "
+            initial={{ opacity: 1 }}
+            animate={{
+              opacity: 0,
+              transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
+            }}
+          />
         </div>
       </AnimatePresence>
     </>
