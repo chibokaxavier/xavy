2d5a0b34ae9666c0b9d7584456a29065d25a98cb
manlikexavy <chibokaxavier@gmail.com>
2024-06-28 08:06:13 +0100

    ADDED THE SIDEBAR USING THE UI LIBRARY INSTALLLED

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+"use client";
+import { usePathname } from "next/navigation";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import Link from "next/link";
+import { CiMenuFries } from "react-icons/ci";
 
 const MobileNav = () => {
+  const links = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "services",
+      path: "/services",
+    },
+    {
+      name: "work",
+      path: "/work",
+    },
+    {
+      name: "resume",
+      path: "/resume",
+    },
+    {
+      name: "contact",
+      path: "/contact",
+    },
+  ];
+  const pathName = usePathname();
   return (
-    <div>MobileNav</div>
-  )
-}
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        <div className=""> Logo</div>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
-export default MobileNav
+export default MobileNav;
