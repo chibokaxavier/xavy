c6f47aef4bf15dc16e82a4a3cfe378cc14eb6f2c
manlikexavy <chibokaxavier@gmail.com>
2024-07-03 11:51:39 +0100

    done with the experience ,skills and eduaction section on my resume page up next is the about me section on the resumepage

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -49,7 +49,8 @@ const about = {
 const experience = {
   icon: "/",
   title: "My Experience",
-  description: "",
+  description:
+    "Since starting my journey in 2020, I've developed responsive and user-friendly interfaces using HTML, CSS, and JavaScript. I've also worked with React and Vue.js, building everything from static websites to complex web applications. This experience has sharpened my problem-solving skills and ability to collaborate with design and backend teams. I stay updated with industry trends to deliver high-quality web solutions.",
   items: [
     {
       company: "AroneEnergy",
@@ -71,17 +72,20 @@ const experience = {
 const education = {
   icon: "/",
   title: "My Education",
-  description: "",
+  description:
+    "I graduated from the University of Nigeria with a degree in Metallurgical and Materials Engineering. Despite my engineering background, I have developed a strong proficiency in the tech world. My education has provided me with a solid foundation in problem-solving and analytical thinking, which I've seamlessly applied to my tech endeavors. This unique combination of engineering knowledge and tech skills allows me to approach challenges with a diverse perspective and innovative solutions.",
   items: [
     {
       institution: "University of Nigeria, Nsukka ",
       degree: "Metallurgical and Materials Engineering",
-      duration: "October 2018 - May 202a",
+      duration: "October 2018 - May 2024",
     },
   ],
 };
 const skills = {
   title: "My Skills",
+  description:
+    "I excel in frontend frameworks such as Next.js, Tailwind CSS etc, allowing me to create highly efficient and aesthetically pleasing web applications. My proficiency extends to backend technologies as well, with a solid understanding of Node.js and MongoDB, enabling me to build robust full-stack applications. This combination of skills ensures I can handle both the client and server sides of development effectively.",
   skillist: [
     {
       icon: <FaHtml5 />,
@@ -150,14 +154,100 @@ const Resume = () => {
         >
           <TabsList className="flex flex-col max-w-[380px] w-full mx-auto xl:mx-0 gap-6">
             <TabsTrigger value="Experience">Experience</TabsTrigger>
-            <TabsTrigger value="SKills">Skills</TabsTrigger>
+            <TabsTrigger value="Skills">Skills</TabsTrigger>
             <TabsTrigger value="Education">Education</TabsTrigger>
             <TabsTrigger value="About me">About me</TabsTrigger>
           </TabsList>
 
           <div className="min-h-[70vh] w-full">
             <TabsContent value="Experience" className="w-full">
-              Experienc
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="text-white/80 mx-auto xl:mx-0 max-w-[600px]">
+                  {experience.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {experience.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 flex flex-col rounded-xl justify-center items-center xl:items-start"
+                        >
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px]">
+                            {item.position}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            <span className="h-[6px] w-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60"> {item.company}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="Skills" className="w-full h-full">
+              <div className="flex flex-col  gap-[30px]">
+                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                  <h3 className="text-4xl font-bold">{skills.title}</h3>
+                  <p className="max-w-[600px] mx-auto xl:mx-0 text-white/60">
+                    {skills.description}
+                  </p>
+                </div>
+                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:gap-[30px] gap-4  ">
+                  {skills.skillist.map((skill, index) => {
+                    return (
+                      <li key={index}>
+                        <TooltipProvider delayDuration={100}>
+                          <Tooltip>
+                            <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                              <div className="text-6xl group-hover:text-accent transition-all duration-300">
+                                {skill.icon}
+                              </div>
+                            </TooltipTrigger>
+                            <TooltipContent>{skill.name}</TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>{" "}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </TabsContent>
+            <TabsContent value="Education" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{education.title}</h3>
+                <p className="text-white/80 mx-auto xl:mx-0 max-w-[600px]">
+                  {education.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {education.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 flex flex-col rounded-xl justify-center items-center lg:items-start gap-1"
+                        >
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px]">
+                            {item.degree}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            <span className="h-[6px] w-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60"> {item.institution}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="About me" className="w-full">
+              About me
             </TabsContent>
           </div>
         </Tabs>
