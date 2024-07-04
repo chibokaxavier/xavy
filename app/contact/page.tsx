e48bab6028326a4e3ea5a791e4aadbe8682940d0
manlikexavy <chibokaxavier@gmail.com>
2024-07-04 17:13:44 +0100

    installed swiper js for the works section

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const contact = () => {
   return (
-    <div>contact page</div>
+    <div>
+      
+    </div>
   )
 }
 
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+"use client";
+import { motion } from "framer-motion";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import Link from "next/link";
+import Image from "next/image";
 
 const work = () => {
-    return (
-        <div>work</div>
-    )
-}
+  return <div>work</div>;
+};
 
-export default work
+export default work;
