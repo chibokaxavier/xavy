8ee45e31245930199757b45d0219bac9668b71f2
manlikexavy <chibokaxavier@gmail.com>
2025-04-23 21:22:44 +0100

    added projects

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -19,26 +19,27 @@ const Work = () => {
     {
       num: "01",
       category: "Fullstack",
-      title: "CareConnect",
+      title: "Kriapay",
       description:
-        "CareConnect is an innovative web application designed to streamline the process of booking medical appointments, connecting doctors and patients seamlessly. The platform allows both individual practitioners and hospitals to manage appointments efficiently, offering a flexible solution for private or institutional use.",
+        "Kriapay is a fintech company that facilitates secure and seamless cross-border payments between African countries. The platform enables businesses and individuals to transact efficiently while ensuring compliance with financial regulations.",
       stack: [
         { name: "Next.js" },
         { name: "Node.js" },
+        { name: "Redux Toolkit" },
         { name: "Tailwind CSS" },
         { name: "MongoDB" },
         { name: "Express" },
       ],
       image: "/careconnect.png",
-      live: "https://care-connect-tan.vercel.app/",
-      github: "https://github.com/chibokaxavier/CareConnect",
+      live: "https://kriapay.com",
+      github: "https://github.com/chibokaxavier",
     },
     {
       num: "02",
       category: "Frontend",
       title: "Arone Energy",
       description:
-        "Arone energy specializes in providing innovative power solutions, with a primary focus on the sale and installation of inverters, solar energy systems, and related power equipment. The company is dedicated to helping residential, commercial, and industrial clients overcome energy challenges by offering reliable, sustainable, and cost-effective power backup systems.",
+        "Arone energy specializes in providing innovative power solutions, with a primary focus on the sale and installation of inverters, solar energy systems, and related power equipment. The company is dedicated to helping residential, commercial, and industrial clients overcome energy challenges by offering reliable, sustainable, and cost-effective power backup systems. I Redesigned the company website to enhance user experience and navigation.",
       stack: [{ name: "Html" }, { name: "Jquery" }, { name: "Tailwind CSS" }],
       image: "/arone.png",
       live: "https://www.aroneenergy.com/",
@@ -46,18 +47,83 @@ const Work = () => {
     },
     {
       num: "03",
-      category: "frontend",
-      title: "Coin Radar",
+      category: "Fullstack",
+      title: "Techtrail",
       description:
-        "A real-time cryptocurrency price tracker built using modern web technologies. The application fetches and displays up-to-date prices for a wide range of cryptocurrencies, providing users with instant access to market data",
+        "TechTrail is a modern Learning Management System (LMS) designed for seamless, interactive learning. From video lessons to quizzes and certifications, it empowers individuals and teams to upskill efficiently. Start your learning journey with TechTrail today!",
       stack: [
-        { name: "Nextjs" },
-        { name: "Firebase" },
+        { name: "Next.js" },
+        { name: "Node.js" },
+        { name: "Redux Toolkit" },
         { name: "Tailwind CSS" },
+        { name: "MongoDB" },
+        { name: "Express" },
       ],
-      image: "/crypto.png",
-      live: "https://crypto-a8eavo7lv-chibokaxavier.vercel.app/",
-      github: "https://github.com/chibokaxavier/CoinRadar",
+      image: "/careconnect.png",
+      live: "https://techtraill.vercel.app",
+      github: "https://github.com/chibokaxavier/techtrail",
+    },
+    {
+      num: "04",
+      category: "Fullstack",
+      title: "Delidish",
+      description:
+        "Delidish is a sleek and modern eCommerce food ordering app that allows users to browse a wide variety of cuisines, place orders effortlessly, and have meals delivered straight to their doorsteps. Designed for convenience, it offers real-time order tracking, secure payment options and personalized user profiles.",
+      stack: [
+        { name: "Next.js" },
+        { name: "Node.js" },
+        { name: "Redux Toolkit" },
+        { name: "Tailwind CSS" },
+        { name: "MongoDB" },
+        { name: "Express" },
+      ],
+      image: "/careconnect.png",
+      live: "https://delidish.vercel.app",
+      github: "https://github.com/chibokaxavier/delidish",
+    },
+    {
+      num: "05",
+      category: "Frontend",
+      title: "Smartvest",
+      description:
+        "A modern, intuitive landing page designed to build trust, showcase opportunities, and guide you to smarter financial moves.",
+      stack: [{ name: "Next.js" }, { name: "Tailwind CSS" }],
+      image: "/careconnect.png",
+      live: "https://smartvest.vercel.app",
+      github: "https://github.com/chibokaxavier/smartvest",
+    },
+    {
+      num: "06",
+      category: "Frontend",
+      title: "Tooniverse",
+      description:
+        "Dive into a sleek landing page, animated world built for gamers. Smooth transitions, bold visuals, and instant access to the hottest titles — all in one epic hub.",
+      stack: [{ name: "Next.js" }, { name: "Tailwind CSS" }],
+      image: "/careconnect.png",
+      live: "https://tooniverse.vercel.app",
+      github: "https://github.com/chibokaxavier/tooniverse",
+    },
+    {
+      num: "07",
+      category: "Frontend",
+      title: "Metaverse",
+      description:
+        "Step beyond the screen into a fully immersive landing page. With sleek animations and next-gen design, this is your gateway to experiences that blur the line between real and virtual.",
+      stack: [{ name: "Next.js" }, { name: "Tailwind CSS" }],
+      image: "/careconnect.png",
+      live: "https://metaverse-blond-phi.vercel.app",
+      github: "https://github.com/chibokaxavier/metaverse",
+    },
+    {
+      num: "08",
+      category: "Frontend",
+      title: "Travel-Hive",
+      description:
+        "A modern travel hub with smooth animations and seamless booking. Discover new destinations, curated experiences, and the journey of a lifetime — all in one place.",
+      stack: [{ name: "Next.js" }, { name: "Tailwind CSS" }],
+      image: "/careconnect.png",
+      live: "https://travelhive-nu.vercel.app",
+      github: "https://github.com/chibokaxavier/travel-hive",
     },
   ];
 
@@ -75,9 +141,9 @@ const Work = () => {
       }}
       className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
-      <div className="container mx-auto">
+      <div className="container mx-auto ">
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
-          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between  xl:order-none">
             <div className="flex flex-col gap-[30px] h-[50%]">
               <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
                 {project.num}
@@ -98,8 +164,12 @@ const Work = () => {
                 })}
               </ul>
               <div className="border border-white/20"></div>
-              <div className="flex items-center gap-4 ">
-                <Link href={project.live}>
+              <div className="flex items-center gap-4  ">
+                <Link
+                  href={project.live}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <TooltipProvider delayDuration={100}>
                     <Tooltip>
                       <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group ">
@@ -111,7 +181,7 @@ const Work = () => {
                     </Tooltip>
                   </TooltipProvider>
                 </Link>
-                <Link href={project.github}>
+                <a href={project.github} target="_blank">
                   <TooltipProvider delayDuration={100}>
                     <Tooltip>
                       <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group ">
@@ -122,7 +192,7 @@ const Work = () => {
                       </TooltipContent>
                     </Tooltip>
                   </TooltipProvider>
-                </Link>
+                </a>
               </div>
             </div>
           </div>
@@ -130,22 +200,22 @@ const Work = () => {
             <Swiper
               slidesPerView={1}
               spaceBetween={30}
-              className="xl:h-[520px] mb-12"
+              className=""
               onSlideChange={handleSlideChange}
             >
               {projects.map((item, index) => {
                 return (
                   <SwiperSlide key={index} className="w-full">
-                    <div className="h-[460px] relative group justify-center items-center bg-transparent">
-                      <div className="absolute top-0 bottom-0w w-full h-full  z-10 "></div>
+                    <div className="h-[160px] relative group justify-center items-center bg-transparent">
+                      {/* <div className="absolute top-0 bottom-0 w-full h-full  z-10 "></div>
                       <div className="relative w-full h-full">
-                        {/* <Image
+                        <Image
                           src={project.image}
                           fill
                           alt=""
                           className="object-contain "
-                        /> */}
-                      </div>
+                        />
+                      </div> */}
                     </div>
                   </SwiperSlide>
                 );
